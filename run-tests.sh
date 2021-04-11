146e07d2f423d91b1609a652932ed6e6ad2f8966
Andre Peric Tavares <andre.peric.tavares@gmail.com>
2021-04-10 23:24:16 -0300

    Do not download iedit for running tests
    `lispy-lite` does not depend on it anymore.

--- run-tests.sh
+++ run-tests.sh
@@ -11,9 +11,6 @@ cp lispy-test.el "$WORKDIR"
 cd "$WORKDIR"
 pwd "$WORKDIR"
 
-$CURL -O ${GHRAW}/victorhge/iedit/master/iedit.el
-$CURL -O ${GHRAW}/victorhge/iedit/master/iedit-rect.el
-$CURL -O ${GHRAW}/victorhge/iedit/master/iedit-lib.el
 $CURL -O ${GHRAW}/abo-abo/avy/master/avy.el
 $CURL -O ${GHRAW}/abo-abo/swiper/master/swiper.el
 $CURL -O ${GHRAW}/abo-abo/swiper/master/ivy.el
@@ -25,5 +22,5 @@ $CURL -O ${GHRAW}/abo-abo/swiper/master/colir.el
 $CURL -O ${GHRAW}/clojure-emacs/clojure-mode/master/clojure-mode.el
 
 # TODO: Why is it necessary to byte compile lispy.el before running the cmd below?
-emacs -Q --batch -L . -f batch-byte-compile iedit.el iedit-rect.el iedit-lib.el avy.el ivy.el swiper.el hydra.el clojure-mode.el lispy.el
+emacs -Q --batch -L . -f batch-byte-compile avy.el ivy.el swiper.el hydra.el clojure-mode.el lispy.el
 emacs -Q -L . -batch -l ert -l clojure-mode -l lispy-test.el -f ert-run-tests-batch-and-exit
